0c8a95f607924b9c5f2edf52569e1b634934f4e3
artheeson <aartheeson3600@gmail.com>
2025-06-03 12:17:19 +0530

    Delete main/menu.java

--- main/menu.java
+++
@@ -1,8 +0,0 @@
-public class menu {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int sum = a + b;
-        System.out.println("The sum of " + a + " and " + b + " is " + sum);
-    }
-}
