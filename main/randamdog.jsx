f15836c63f02965c38e8301638d1618079923924
artheeson <aartheeson3600@gmail.com>
2025-06-03 12:35:10 +0530

    Update and rename sample2.jsx to randamdog.jsx

--- main/randamdog.jsx
+++ main/randamdog.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react";
 
-export default function sample2(){
+export default function randomdog(){
     const [imgUrl, setImgUrl] = useState(null);
 
     async function fetchDogPhoto() {
