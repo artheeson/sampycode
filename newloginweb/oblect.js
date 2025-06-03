8416e02dc8c90a6deb3973f9545196cfd00411b9
artheeson <aartheeson3600@gmail.com>
2025-06-03 12:12:52 +0530

    Delete newloginweb directory

--- newloginweb/oblect.js
+++
@@ -1,8 +0,0 @@
-function sendOTP() {
-    let mobile = document.getElementById("mobile").value;
-    if(mobile.length == 10) {
-        alert("OTP Sent to " + mobile);
-    } else {
-        alert("Enter a valid 10-digit mobile number");
-    }
-}
